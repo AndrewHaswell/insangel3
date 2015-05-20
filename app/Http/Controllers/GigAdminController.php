98f90f80370c099f63d62a313105f549d2c4e363
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2015-05-20 21:48:58 +0100

    Updated

--- app/Http/Controllers/GigAdminController.php
+++ app/Http/Controllers/GigAdminController.php
@@ -4,92 +4,94 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Band;
 
+use App\Venue;
 use Request;
 
-class GigAdminController extends Controller {
+class GigAdminController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function index()
+  {
+    //
+  }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create()
+  {
     $band = Band::findorfail(1);
-    return $band;
+    $venues = Venue::all();
 
-		return view('admin.gig.create');
-	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
 
-	}
+    return view('admin.gig.create', compact('band', 'venues'));
+  }
 
-  public function confirm(){
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store()
+  {
+  }
+
+  public function confirm()
+  {
     $input = Request::all();
 
     return $input;
   }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+  /**
+   * Display the specified resource.
+   *
+   * @param  int $id
+   * @return Response
+   */
+  public function show($id)
+  {
+    //
+  }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int $id
+   * @return Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int $id
+   * @return Response
+   */
+  public function update($id)
+  {
+    //
+  }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
