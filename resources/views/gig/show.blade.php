75410f9086cb59b79ec36f32192904d3e05159a9
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2015-05-25 10:33:40 +0100

    Finished basic gig creation and editing

--- resources/views/gig/show.blade.php
+++ resources/views/gig/show.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Gig Guide</title>
 
+    <link href="{{ URL::asset('css/main.css') }}" rel="stylesheet" type="text/css">
     <style>
         body {
             font-family: "verdana", "sans-serif";
@@ -32,43 +33,65 @@
     </style>
 </head>
 <body>
-
-
-@if (!empty($gigs))
-    @foreach ($gigs as $gig)
-        <div class="gig">
-            <div class="title">{{ $gig['title'] ? $gig['title'] . ' :: ': '' }}{{Carbon\Carbon::parse($gig['datetime'])->format('D jS M y')}}</div>
-            @if (!empty($gig['subtitle']))
-                <div class="subtitle">{{$gig['subtitle']}}</div>
-            @endif
-            <div class="venue">{{$gig['venue']['venue_name']}}</div>
-            <div class="bands">
-                <?php $band_count = count($gig['bands']); ?>
-                <?php $counter = 1; ?>
-                @foreach ($gig['bands'] as $band)
-                    <span class="band"><a href="#">{{$band['band_name']}}</a></span>
-                    @if ($counter < $band_count)
-                        <span class="band_separator">-</span>
-                    @endif
-                    <?php $counter++; ?>
-                @endforeach
-            </div>
-            <div class="details">
-                @if (!empty($gig['notes']))
-                    <span class="detail">{{$gig['notes']}}</span>
-                @endif
-                @if (!empty($gig['cost']))
-                    <span class="detail">{{is_numeric($gig['cost']) ? '&pound;' . number_format($gig['cost'], 2) : $gig['cost']}}</span>
-                @endif
-                <span class="detail">{{Carbon\Carbon::parse($gig['datetime'])->format('g:i a')}}</span>
-                <span class="detail-last">07901 616 185</span>
-
-
-            </div>
+<div id="head">
+    <div id="insangel_logo"><img src="/images/insangel.png"/></div>
+</div>
+<div id="main">
+    <div id="side_bar">
+        <div id="side_bar_text">
+            <h1>Cover Gigs and Venues</h1>
         </div>
+    </div>
+    <div id="body">
+        <div id="body_text">
 
-    @endforeach
-@endif
 
+            @if(Session::has('message'))
+                <p class="alert alert-danger">{{ Session::get('message') }}</p>
+            @endif
+
+
+            @if (!empty($gigs))
+                @foreach ($gigs as $gig)
+                    <div class="gig">
+                        <div class="gig_title">{{ $gig['title'] ? $gig['title'] . ' :: ': '' }}{{Carbon\Carbon::parse($gig['datetime'])->format('D jS M y')}}</div>
+                        @if (!empty($delete))
+                            <span class="delete"><a href="#">Delete Gig</a></span>
+                            <span class="edit"><a href="/admin/gig/{{$gig['id']}}/edit">Edit Gig</a></span>
+                        @endif
+                        @if (!empty($gig['subtitle']))
+                            <div class="subtitle">{{$gig['subtitle']}}</div>
+                        @endif
+                        <div class="venue">{{$gig['venue']['venue_name']}}</div>
+                        <div class="bands">
+                            <?php $band_count = count($gig['bands']); ?>
+                            <?php $counter = 1; ?>
+                            @foreach ($gig['bands'] as $band)
+                                <span class="band"><a href="#">{{$band['band_name']}}</a></span>
+                                @if ($counter < $band_count)
+                                    <span class="band_separator">-</span>
+                                @endif
+                                <?php $counter++; ?>
+                            @endforeach
+                        </div>
+                        <div class="details">
+                            @if (!empty($gig['notes']))
+                                <span class="detail">{{$gig['notes']}}</span>
+                            @endif
+                            @if (!empty($gig['cost']))
+                                <span class="detail">{{is_numeric($gig['cost']) ? '&pound;' . number_format($gig['cost'], 2) : $gig['cost']}}</span>
+                            @endif
+                            <span class="detail">{{Carbon\Carbon::parse($gig['datetime'])->format('g:i a')}}</span>
+                            <span class="detail-last">07901 616 185</span>
+
+
+                        </div>
+                    </div>
+
+                @endforeach
+            @endif
+        </div>
+    </div>
+</div>
 </body>
 </html>
