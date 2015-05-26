2cff4f983784dfc0bd0e7fa5c0ef294bcbd200bd
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2015-05-26 23:18:08 +0100

    Rename attempt

---
+++ app/Http/Controllers/BandController.php
@@ -0,0 +1,20 @@
+<?php namespace App\Http\Controllers;
+
+use App\Band;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class BandController extends Controller {
+
+  /**
+   * @author Andrew Haswell
+   */
+  public function index(){
+    $bands = Band::AllCurrentByDate()->get();
+    return $bands;
+
+	}
+
+}
--- app/band_.php
+++ app/band_.php
@@ -1,5 +1,6 @@
 <?php namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Band extends Model
@@ -11,6 +12,7 @@ class Band extends Model
   /**
    * @param $value
    * @author Andrew Haswell
+   *         
    */
 
   public function setBandNameAttribute($value)
@@ -24,7 +26,7 @@ class Band extends Model
    */
   public function gigs()
   {
-    return $this->belongsToMany('App\Gig')->withTimestamps();
+    return $this->belongsToMany('App\Gig');
   }
 
   /**
@@ -34,5 +36,15 @@ class Band extends Model
   public function images()
   {
     return $this->hasMany('App\Band_image');
+  }
+
+  /**
+   * @param $query
+   * @return mixed
+   * @author Andrew Haswell
+   */
+  public function scopeAllCurrentByDate($query)
+  {
+    return $query->with('gigs', 'gigs.venue')->orderBy('band_name', 'asc');
   }
 }
