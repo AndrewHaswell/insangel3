54352cf8b1c00651103d40efaa81eb5490856e9a
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2016-03-17 20:16:54 +0000

    Annoying date functions

--- app/Http/Controllers/UploadAdminController.php
+++ app/Http/Controllers/UploadAdminController.php
@@ -3,82 +3,129 @@
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+use Monolog\Logger;
+
 use Illuminate\Http\Request;
 
-class UploadAdminController extends Controller {
+class UploadAdminController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-    return view('gig.upload');
-	}
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function index()
+  {
+    return view('admin.gig.upload');
+  }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create()
+  {
+    //
+  }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store()
+  {
+    echo '<pre>';
+    $gig_data = Input::all();
+    $gig_data['notes'];
+    $gig_data = explode("\n\r", $gig_data['notes']);
+    foreach ($gig_data as $gig) {
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+      var_dump($gig);
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+      $gig_details = explode("\n", $gig);
+      if (count($gig_details) < 3) {
+        continue;
+      } else {
+        $gig_details = array_map('trim', $gig_details);
+      }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
 
+      $gig_extra_details = array_map('trim', explode('|', array_pop($gig_details)));
+
+      array_pop($gig_extra_details);// Dump the phone number
+
+      $gig_time = $this->fix_time(array_pop($gig_extra_details));
+      $gig_cost = array_pop($gig_extra_details);
+      $gig_date = date('Y-m-d', strtotime(array_shift($gig_details))) . 'T' . $gig_time;
+
+      var_dump($gig_details);
+      var_dump($gig_cost);
+      var_dump($gig_date);
+
+      exit();
+    }
+    exit;
+  }
+
+  private function fix_time($time)
+  {
+    $pm = strpos($time, 'pm') !== false ? true : false;
+    $time = str_pad(preg_replace('/[^0-9]*/i', '', $time), 5, '0', STR_PAD_RIGHT);
+    $time = $pm ? $time + 120000 : str_pad($time, 6, '0', STR_PAD_LEFT);
+    return implode(':', str_split($time, 2));
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int $id
+   *
+   * @return Response
+   */
+  public function show($id)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int $id
+   *
+   * @return Response
+   */
+  public function edit($id)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int $id
+   *
+   * @return Response
+   */
+  public function update($id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int $id
+   *
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
--- resources/views/admin/gig/upload.blade.php
+++ resources/views/admin/gig/upload.blade.php
@@ -2,24 +2,24 @@
 
 @section('form_body')
 
-    {!! Form::open(['action' => 'UploadAdminController@store']) !!}
+  {!! Form::open(['action' => 'UploadAdminController@store']) !!}
+
+  <div class="form-group">
+    {!! Form::label('notes', 'Notes: ') !!}
+    {!! Form::textarea('notes', null, ['class'=>'form-control no_editor', 'rows'=>'5']) !!}
+  </div>
+
+  <div class="form-group">
+    {!! Form::label('cover', 'Cover Gig?') !!}
+    {!! Form::checkbox('cover', 1, (!empty($gigs['cover']) && $gigs['cover'] == 'Y' ? true : false) ) !!}
+  </div>
 
 
+  <div class="form-group">
+    {!! Form::submit( 'Upload Gig List', ['class' => 'btn btn-primary form-control']) !!}
+  </div>
 
-
-
-    <div class="form-group">
-        {!! Form::label('notes', 'Notes: ') !!}
-        {!! Form::text('notes', (!empty($gigs['notes']) ? $gigs['notes'] : null), ['class'=>'form-control']) !!}
-    </div>
-
-
-
-    <div class="form-group">
-        {!! Form::submit( 'Upload Gig List', ['class' => 'btn btn-primary form-control']) !!}
-    </div>
-
-    {!! Form::close() !!}
+  {!! Form::close() !!}
 
 
 
